f606a3567ac87cbd23458603c1c35cb1cb341740
sleyaa <zarekparchmont14@gmail.com>
2024-09-17 17:06:38 -0600

    Add files via upload

---
+++ practica2.js
@@ -0,0 +1,17 @@
+function calcularFactorial(n){
+    if (n === 0 || n === 1){
+        return 1;
+    }
+
+    let resultado = 1;
+    for (let i = n; i > 1; i--){
+        resultado *= i;
+    }
+    return resultado;
+}
+
+function mostrarFactoriales(){
+    const numero = parseInt(document.getElementById('numero').value);
+    const resultado = calcularFactorial(numero);
+    document.getElementById('resultado').textContent = `El factorial de ${numero} es ${resultado}`;
+}
